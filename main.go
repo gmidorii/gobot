e2519234aea6470441dbf44872679ef49f0bef04
tagasouichiro <stitow14@gmail.com>
2017-04-03 22:39:25 +0900

    add slack bot and word post

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 
 	"io/ioutil"
 
+	"bytes"
+
 	"github.com/BurntSushi/toml"
+	"github.com/nlopes/slack"
 )
 
 type Release struct {
@@ -15,16 +18,65 @@ type Release struct {
 	Day  string
 }
 
+type Config struct {
+	Token string
+}
+
 func main() {
-	args, err := ioutil.ReadFile("args.toml")
+	f, err := ioutil.ReadFile("config.toml")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	var release Release
-	if _, err := toml.Decode(string(args), &release); err != nil {
+	var config Config
+	if _, err := toml.Decode(string(f), &config); err != nil {
 		log.Fatalln(err)
 	}
+	api := slack.New(config.Token)
+	os.Exit(run(api))
+}
 
-	t, _ := template.ParseFiles("template.txt")
-	t.Execute(os.Stdout, release)
+func run(api *slack.Client) int {
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			switch ev := msg.Data.(type) {
+			case *slack.MessageEvent:
+				switch ev.Text {
+				case "mocha-release":
+					post, err := post()
+					if err != nil {
+						log.Println(err)
+					}
+					rtm.SendMessage(rtm.NewOutgoingMessage(post, ev.Channel))
+				default:
+					log.Println("No response")
+				}
+			case *slack.InvalidAuthEvent:
+				log.Println("Error")
+				return 1
+			}
+		}
+	}
+}
+
+func post() (string, error) {
+	args, err := ioutil.ReadFile("args.toml")
+	if err != nil {
+		return "", err
+	}
+	var release Release
+	if _, err := toml.Decode(string(args), &release); err != nil {
+		return "", err
+	}
+
+	t, err := template.ParseFiles("template.txt")
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	t.Execute(&buf, release)
+	return buf.String(), nil
 }
